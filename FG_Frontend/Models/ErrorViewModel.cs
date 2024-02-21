1be9a40ff1d8f34c3f231ad3d9b4f035894beac5
Vijaygokulraj <vijaygokulraj13@gmail.com>
2024-02-21 10:48:24 +0530

    Add project files.

---
+++ FG_Frontend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FG_Frontend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
